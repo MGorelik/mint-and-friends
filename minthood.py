ac3b19554503ab5ec58de03da874e4636dfcdecf
mgorelik <michael.gorelik1@gmail.com>
2018-07-15 15:33:02 -0400

    coinbase account creation working

--- minthood.py
+++ minthood.py
@@ -2,16 +2,21 @@ from getpass import getpass
 
 import keyring
 
+from coinbase_api.api import CoinbaseApi
 from mint.api import MintApi
 from robinhood.api import RobinhoodApi
 
 
-def main():
-    username = input("Robinhood username: ")
-    password = keyring.get_password("minthood-robinhood", username)
+def main(*args):
+    if args and args[0] == 'coinbase':
+        keyring.delete_password('minthood-coinbase-api', 'coinbase')
+        keyring.delete_password('minthood-coinbase-secret', 'coinbase')
+
+    username = input('Robinhood username: ')
+    password = keyring.get_password('minthood-robinhood', username)
 
     if not password:
-        password = getpass("Robinhood password: ")
+        password = getpass('Robinhood password: ')
 
     robinhood = RobinhoodApi(username, password)
 
@@ -19,7 +24,7 @@ def main():
 
     if login:
         # store valid creds
-        keyring.set_password("minthood-robinhood", username, password)
+        keyring.set_password('minthood-robinhood', username, password)
 
         # get accounts
         robinhood_accounts = robinhood.get_accounts()
@@ -34,12 +39,12 @@ def main():
             portfolio_values[account.get('account_number')] = portfolio_value
 
         # update the corresponding account in Mint
-        username = input("Mint username: ")
+        username = input('Mint username: ')
 
-        password = keyring.get_password("minthood-mint", username)
+        password = keyring.get_password('minthood-mint', username)
 
         if not password:
-            password = getpass("Mint password: ")
+            password = getpass('Mint password: ')
 
         try:
             mint = MintApi(username, password)
@@ -47,7 +52,25 @@ def main():
             raise e
 
         # store valid creds
-        keyring.set_password("minthood-mint", username, password)
+        keyring.set_password('minthood-mint', username, password)
+
+        # log into coinbase
+        api_key = keyring.get_password('minthood-coinbase-api', 'coinbase')
+
+        if not api_key:
+            api_key = input('Coinbase API key: ')
+
+        api_secret = keyring.get_password('minthood-coinbase-secret', 'coinbase')
+
+        if not api_secret:
+            api_secret = input('Coinbase API secret: ')
+
+        try:
+            coinbase = CoinbaseApi(api_key, api_secret)
+            keyring.set_password('minthood-coinbase-api', 'coinbase', api_key)
+            keyring.set_password('minthood-coinbase-secret', 'coinbase', api_secret)
+        except Exception as e:
+            raise e
 
         mint_accounts = mint.get_accounts()
 
@@ -62,21 +85,53 @@ def main():
                 for mn_account in mint_rb_accounts:
                     mn_name = mn_account.get('name')
 
-                    if rb_name == mn_name:
+                    if f'{ROBINHOOD_PREFIX}{rb_name}' == mn_name:
                         exists = True
                 if not exists:
                     try:
-                        created = mint.create_property_account(f"{ROBINHOOD_PREFIX}{rb_name}", portfolio_values[rb_name])
+                        created = mint.create_property_account(f'{ROBINHOOD_PREFIX}{rb_name}', portfolio_values[rb_name])
                         if created.get('success'):
-                            print(f"Created Mint account for Robinhood account {rb_name}")
+                            print(f'Created Mint account for Robinhood account {rb_name}')
                         else:
                             print(f"Problem creating Mint account for Robinhood account {rb_name}: {created.get('error')}")
+                    except Exception as e:
+                        raise e
+
+        # do the same for coinbase
+        cb_accounts = coinbase.get_accounts()
+        coinbase_portfolio = {}
+        for cb_account in cb_accounts:
+            coinbase_portfolio[f'{cb_account.balance.currency}-USD'] = cb_account
+
+        COINBASE_PREFIX = 'Coinbase-'
+        mint_cb_accounts = [mn for mn in mint_accounts if COINBASE_PREFIX in mn.get('name')]
+
+        if len(cb_accounts) > len(mint_cb_accounts):
+            for cb_account in cb_accounts:
+                cb_name = f'{cb_account.balance.currency}-USD'
+                exists = False
+                for mn_account in mint_cb_accounts:
+                    mn_name = mn_account.get('name')
+
+                    if f'{COINBASE_PREFIX}{cb_name}' == mn_name:
+                        exists = True
+                if not exists:
+                    try:
+                        value = coinbase.get_account_value(cb_account)
+                        if value > 0:
+                            created = mint.create_property_account(f'{COINBASE_PREFIX}{cb_name}', value)
+                            if created.get('success'):
+                                print(f'Created Mint account for Coinbase account {cb_name}')
+                            else:
+                                print(
+                                    f"Problem creating Mint account for Coinbase account {cb_name}: {created.get('error')}")
                     except Exception as e:
                         raise e
 
         for account in mint_accounts:
             account_name = account.get('name')
 
+            # update robinhood
             idx = account_name.find(ROBINHOOD_PREFIX)
             account_num = account_name[idx+len(ROBINHOOD_PREFIX):] if idx > -1 else ''
 
@@ -84,9 +139,47 @@ def main():
                 updated = mint.set_property_account_value(account, portfolio_values.get(account_num))
 
                 if updated.get('success'):
-                    print(f"Updated value for account {account_name}")
+                    print(f'Updated value for account {account_name}')
+                else:
+                    print(f"Problem updating account {account_name}: {updated.get('error')}")
+
+            # update coinbase
+            idx = account_name.find(COINBASE_PREFIX)
+            account_num = account_name[idx+len(COINBASE_PREFIX):] if idx > -1 else ''
+
+            if COINBASE_PREFIX in account_name and account_num:
+                updated = mint.set_property_account_value(account, coinbase_portfolio.get(account_num))
+                if updated.get('success'):
+                    print(f'Updated value for account {account_name}')
                 else:
                     print(f"Problem updating account {account_name}: {updated.get('error')}")
 
 
+def test_coinbase():
+    api_key = keyring.get_password('minthood-coinbase-api', 'coinbase')
+
+    if not api_key:
+        api_key = input('Coinbase API key: ')
+
+    api_secret = keyring.get_password('minthood-coinbase-secret', 'coinbase')
+
+    if not api_secret:
+        api_secret = input('Coinbase API secret: ')
+
+    try:
+        coinbase = CoinbaseApi(api_key, api_secret)
+        keyring.set_password('minthood-coinbase-api', 'coinbase', api_key)
+        keyring.set_password('minthood-coinbase-secret', 'coinbase', api_secret)
+    except Exception as e:
+        raise e
+
+    accounts = coinbase.get_accounts()
+
+    for account in accounts.data:
+        balance = account.balance
+        print('%s: %s %s' % (account.name, balance.amount, balance.currency))
+        value = coinbase.get_account_value(account)
+        print('val: ', value)
+
 main()
+# test_coinbase()
